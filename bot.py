d5c38edf995340a34cadabe8e93a8b3e817fa23e
Vitor S. <svavitor.dev@gmail.com>
2024-10-28 10:09:30 -0300

    testando comando proxima, codigo precisa de melhorias

--- bot.py
+++ bot.py
@@ -5,9 +5,7 @@ import logging
 from dotenv import load_dotenv
 import requests
 from datetime import datetime
-from zoneinfo import ZoneInfo
 import pytz
-
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -19,6 +17,19 @@ logging.basicConfig(
 url = "https://api.jolpi.ca/ergast/f1/2024/21.json"
 
 logger = logging.getLogger(__name__)
+
+def stringParaLocalTime(time_string):
+        utc_time = datetime.strptime(time_string, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=pytz.UTC)
+        local_time = utc_time.astimezone(pytz.timezone("America/Sao_Paulo"))
+        local_time = local_time.strftime("%d/%m/%Y, %H:%M")
+        return local_time
+
+def retornaDataString(evento):
+        time_string = f"{evento['date']}T{evento['time']}"
+        utc_time = datetime.strptime(time_string, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=pytz.UTC)
+        local_time = utc_time.astimezone(pytz.timezone("America/Sao_Paulo"))
+        local_time = local_time.strftime("%d/%m/%Y, %H:%M")
+        return local_time
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello")
@@ -35,19 +46,49 @@ async def proxima(update: Update, context: ContextTypes.DEFAULT_TYPE):
         dia = next_round['Races'][0]['date']
         hora = next_round['Races'][0]['time']
 
+        fp1 = next_round['Races'][0]['FirstPractice']
+        fp2 = 0
+        fp3 = 0
+        sprint_quali = 0
+        sprint = 0
+
+        if "SecondPractice" in next_round['Races'][0]:
+            fp2 = next_round['Races'][0]['SecondPractice']
+        if "ThirdPractice" in next_round['Races'][0]:
+            fp3 = next_round['Races'][0]['ThirdPractice']
+        if "SprintQualifying" in next_round['Races'][0]:
+            sprint_quali = next_round['Races'][0]['SprintQualifying']
+            sprint = next_round['Races'][0]['Sprint']
+        quali = next_round['Races'][0]['Qualifying']
+        
+        fp1_data = f"{fp1['date']}T{fp1['time']}"
+        quali_data = f"{quali['date']}T{quali['time']}"
+
+        fp1_data = stringParaLocalTime(fp1_data)
+        quali_data = stringParaLocalTime(quali_data)
+
         utc_string = f"{dia}T{hora}"
 
-        utc_time = datetime.strptime(utc_string, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=pytz.UTC)
-
-        local_time = utc_time.astimezone(pytz.timezone("America/Sao_Paulo"))
-        local_time = local_time.strftime("%d/%m/%Y, %H:%M")
+        local_time = stringParaLocalTime(utc_string)
 
         message = (
-            f"{ nome }\n"
-            f"{ circuito }\n"
-            f"{ local_time }\n"
-        )
-    await update.message.reply_text(message)
+            f"<b>{ nome }</b>\n"
+            f"{ circuito }\n\n"
+        )   
+
+        message+= f"<b>FP1:</b> { fp1_data }\n\n"
+        if fp2:
+            message+= f"<b>FP2:</b> {retornaDataString(fp2)}\n\n"
+        if fp3:
+            message+= f"<b>FP3:</b> {retornaDataString(fp3)}\n\n"
+        if sprint:
+            message+= f"<b>SprintQuali:</b> {retornaDataString(sprint_quali)}\n\n"
+            message+= f"<b>Sprint:</b> {retornaDataString(sprint)}\n\n"
+        message+= f"<b>Quali:</b> { quali_data }\n\n"
+
+        message+= f"<b>Corrida:</b> { local_time }"
+
+    await update.message.reply_text(message, parse_mode='HTML')
 
 def main():
     application = ApplicationBuilder().token(BOT_TOKEN).build()
