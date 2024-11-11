9d83a683eff2c3f972b649f5ff81baeba11543d3
Vitor S. <svavitor.dev@gmail.com>
2024-11-11 06:40:29 -0300

    melhorando legibilidade e removendo print desnecessario

--- comandos/drivers.py
+++ comandos/drivers.py
@@ -7,7 +7,7 @@ async def drivers(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     message = "<b>Campeonato de Pilotos</b>\n"
     for piloto in pilotos:
-        print(piloto)
-        message += f"<b>{ piloto['position'] } - { piloto['Driver']['givenName'] } { piloto['Driver']['familyName'] } - { piloto['points'] } </b>\n"
+        nome_completo = f'{ piloto['Driver']['givenName'] } { piloto['Driver']['familyName'] }'
+        message += f"<b>{ piloto['position'] } - { nome_completo } - { piloto['points'] } </b>\n"
 
     await update.message.reply_text(message, parse_mode='HTML')
