14709f0c26b82a5a9e386cbf11fb250d865f32f5
Vitor S. <svavitor.dev@gmail.com>
2026-09-01 06:44:19 -0300

    feat: padroniza mensagens dos comandos

--- comandos/drivers.py
+++ comandos/drivers.py
@@ -1,23 +1,16 @@
+from comandos.mensagens import ERRO_CONSULTA, classificacao_pilotos
 from servicos.campeonato_pilotos import campeonato_pilotos
 from telegram.ext import ContextTypes
 from datetime import datetime
 from telegram import Update
 
+
 async def drivers(update: Update, context: ContextTypes.DEFAULT_TYPE):
     pilotos = campeonato_pilotos()
-    
     if pilotos is None:
-        await update.message.reply_text("Não foi possível consultar os dados.", parse_mode='HTML')
-
+        await update.message.reply_text(ERRO_CONSULTA)
+        return
     ano_atual = datetime.now().year
-    print(pilotos)
-    message = f"<b>Campeonato de Pilotos - {ano_atual}</b>\n"
-
-    ind = 1
-    #piloto['position']
-    for piloto in pilotos:
-        nome_completo = f"{ piloto['Driver']['givenName'] } { piloto['Driver']['familyName'] }"
-        message += f"{ ind } - { nome_completo } - { piloto['points'] }\n"
-        ind = ind+1
-
-    await update.message.reply_text(message, parse_mode='HTML')
+    await update.message.reply_text(
+        classificacao_pilotos(pilotos, ano_atual), parse_mode='HTML'
+    )
--- comandos/teams.py
+++ comandos/teams.py
@@ -1,19 +1,16 @@
+from comandos.mensagens import ERRO_CONSULTA, classificacao_construtores
 from servicos.campeonato_construtores import campeonato_construtores
 from telegram.ext import ContextTypes
 from telegram import Update
 from datetime import datetime
 
+
 async def teams(update: Update, context: ContextTypes.DEFAULT_TYPE):
     times = campeonato_construtores()
-
     if times is None:
-        await update.message.reply_text("Não foi possível consultar os dados.", parse_mode='HTML')
-    
+        await update.message.reply_text(ERRO_CONSULTA)
+        return
     ano_atual = datetime.now().year
-
-    message = f"<b>Campeonato de Construtores - {ano_atual}</b>\n"
-    for time in times:
-        nome_time = f"{ time['Constructor']['name'] } "
-        message += f"{ time['position'] } - { nome_time } - { time['points'] }\n"
-
-    await update.message.reply_text(message, parse_mode='HTML')
+    await update.message.reply_text(
+        classificacao_construtores(times, ano_atual), parse_mode='HTML'
+    )
